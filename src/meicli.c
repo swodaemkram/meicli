7d6f8541a2d7240761db8890f6be05a13d8f8948
Mark Meadows <mark@kyin.net>
2019-05-22 14:27:35 -0400

    first commit

---
+++ src/meicli.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : meicli.c
+ Author      : Mark Meadows
+ Version     : 00.00.00
+ Copyright   : Copyright (c) 2019 Fireking Security Group
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("MEI_Service"); /* prints MEI_Service */
+	return EXIT_SUCCESS;
+}
